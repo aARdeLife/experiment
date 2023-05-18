ff97fa93c6668e6328d33f821534d29693367149
Super Vision <107816707+aARdeLife@users.noreply.github.com>
2023-05-18 16:29:20 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,24 +1,26 @@
-navigator.mediaDevices.getUserMedia({ video: { facingMode: 'environment' } })
-    .then(function(stream) {
-        var video = document.querySelector('video');
-        video.srcObject = stream;
-        video.onloadedmetadata = function(e) {
-            video.play();
-        };
-    })
-    .catch(function(err) {
-        console.log(err.name + ": " + err.message);
-    });
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <title>GeoAR.js demo</title>
+    <script src="https://aframe.io/releases/1.3.0/aframe.min.js"></script>
+    <script src="https://unpkg.com/aframe-look-at-component@0.8.0/dist/aframe-look-at-component.min.js"></script>
+    <script src="https://raw.githack.com/AR-js-org/AR.js/master/aframe/build/aframe-ar-nft.js"></script>
+  </head>
 
-document.getElementById('capture').addEventListener('click', function() {
-    var video = document.querySelector('video');
-    var canvas = document.createElement('canvas');
-    canvas.width = video.videoWidth;
-    canvas.height = video.videoHeight;
-    var context = canvas.getContext('2d');
-    context.drawImage(video, 0, 0, canvas.width, canvas.height);
-
-    var img = document.createElement('img');
-    img.src = canvas.toDataURL('image/jpeg');
-    document.getElementById('photos').appendChild(img);
-});
+  <body>
+    <a-scene
+      vr-mode-ui="enabled: false"
+      arjs="sourceType: webcam; videoTexture: true; debugUIEnabled: false;"
+    >
+      <a-gltf-model
+        src="https://github.com/aARdeLife/Bridges/raw/e96f53de46d2c9dc6e6560734ad35a385ea95c18/polforweb%20(3).glb"
+        scale="0.05 0.05 0.05"
+        look-at="[gps-camera]"
+        gps-entity-place="latitude: 53.453403; longitude: -2.299748;"
+      ></a-gltf-model>
+      <a-camera gps-camera rotation-reader> </a-camera>
+    </a-scene>
+  </body>
+</html>
